22c424514b262eb59d9e6c9e4726e8193ab731a6
Pedro <pedro.henriqueph0@outlook.com>
2026-05-15 00:18:28 -0300

    fix allowed hosts

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,8 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   vite: {
     preview: {
-      allowedHosts: ["gabrielpinturas.onrender.com"],
+      host: "0.0.0.0",
+      allowedHosts: true,
     },
   },
 
